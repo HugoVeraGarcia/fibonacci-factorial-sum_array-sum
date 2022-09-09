7bf048de8e4d02f2bc750d0839e031d4db1c680b
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2022-09-09 01:33:41 -0500

    second commit

--- sum.js
+++ sum.js
@@ -1,13 +1,7 @@
 function sum(n) {
   if (n > 0) {
     return n + sum(n - 1);
-    /* const sumReturn = sum(n-1);
-        const sumNumbers = n + sumReturn;
-        return sumNumbers; */
   }
   return n;
 }
-console.log(sum(1));
-console.log(sum(2));
-console.log(sum(3));
 console.log(sum(4));
